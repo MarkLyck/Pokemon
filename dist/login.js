fc1868b0805f51411c4ea0693d237b33b1ce7c6b
Mark Lyck <mark.lyck@gmail.com>
2016-07-10 12:56:03 -0500

    Added animations and sound effect

--- dist/login.js
+++ dist/login.js
@@ -2,15 +2,21 @@ var $loginInput = $('#login-input');
 var $loginBtn = $('#login-btn');
 var $modalContainer = $('.modal-container');
 
+var themeMusic = new Audio('assets/sounds/PokemonTheme.mp3');
+
 if (!sessionStorage.username) {
   $modalContainer.css('display', 'block');
+  themeMusic.play();
 }
 
 $loginBtn.on('click',function(){
   let re = /[a-zA-Z0-9]/g;
   if (re.test($loginInput.val())) {
+    themeMusic.pause();
     sessionStorage.username = $loginInput.val();
+    console.log('USER BEFORE: ', player.userName);
     player.userName = $loginInput.val();
+    console.log('USER AFTER: ', player.userName);
     $modalContainer.css('display','none');
   } else {
     $('.login-modal').effect( "shake" );
